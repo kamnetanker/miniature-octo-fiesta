6373666e16fadb6a22bb6d6d32cde6be4780101a
kamnetanker <kamnetanker3@gmail.com>
2022-12-14 12:41:23 +0300

    added project for testing via mstest

---
+++ formula/formula_proj/Program.cs
@@ -0,0 +1,22 @@
+﻿using System; 
+namespace formula_proj{
+	public class formula_proj{
+		public static void Main(string[] argv){
+			Console.WriteLine("Hello, World!");
+			Console.WriteLine("Function Result: "+F(0.23d).ToString());
+		}
+	
+		public static double F(double x){
+			return SqX(x)+cosX(x)*ExpoX(x);
+		}
+		public static double SqX(double x){
+			return x*x;
+		}
+		public static double cosX(double x){
+			return Math.Cos(x);
+		}
+		public static double ExpoX(double x){
+			return Math.Exp(-2*x);
+		}
+	}
+} 
---
+++ formula/formula_tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using formula_proj;
+
+namespace formula_tests;
+
+[TestClass]
+public class FormulaTest
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
